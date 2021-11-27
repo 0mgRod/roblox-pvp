12fc5984ea5d15663046348cb46bec444783934c
0mgRod <89850217+0mgRod@users.noreply.github.com>
2021-11-27 10:45:06 +0000

    Delete templates/scripts/lobby directory

--- templates/scripts/lobby/StarterPack/Camera.lua
+++
@@ -1,4 +0,0 @@
-wait()
-local cc = workspace.CurrentCamera
-cc.CameraType = Enum.CameraType.Scriptable
-cc.CFrame = workspace.CameraParts_0mgRodsPlugin.Blank.CFrame
--- templates/scripts/lobby/StarterPlayer/StarterPlayerScripts/Mouse.lua
+++
@@ -1,5 +0,0 @@
--- This script is not in use. Use it for custom mouse cursors.
-
-wait()
-local mouse = game.Players.LocalPlayer:GetMouse()
-mouse.Icon = "rbxassetid://6248376394"
