ca2ffdae366bb9257cfff360178801f95c98c538
0mgRod <89850217+0mgRod@users.noreply.github.com>
2021-11-27 10:26:45 +0000

    Delete templates/scripts directory

--- templates/scripts/StarterGui/Coins.lua
+++
@@ -1,9 +0,0 @@
-wait()
-local leaderstats = game.Players.LocalPlayer.leaderstats
-local coins = leaderstats.Coins
-
-script.Parent.Main.CoinCount.Coins.Text = coins.Value
-
-coins:GetPropertyChangedSignal("Value"):Connect(function()
-	script.Parent.Main.CoinCount.Coins.Text = coins.Value
-end)
--- templates/scripts/StarterGui/NoAbilities.lua
+++
@@ -1,3 +0,0 @@
-wait()
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
-game.Players.LocalPlayer.Character.Humanoid.JumpPower = 0
